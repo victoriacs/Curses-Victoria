5f6a1c169727c4071ff5d22a65182b30525357d7
Victoria <v.carreto@sapalomera.cat>
2021-11-02 19:18:06 +0100

    JavaDoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bicicleta"},{"p":"<Unnamed>","l":"ConfigGame"},{"p":"<Unnamed>","l":"Cotxe"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Pilot"},{"p":"<Unnamed>","l":"Vehicle"}];updateSearchResults();
